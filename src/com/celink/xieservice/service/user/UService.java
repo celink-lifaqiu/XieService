fbc4ec1ade141c66829687c2c0882b07f0364a1d
yejianlin <rd53@ce-link.com>
2015-07-11 17:53:32 +0800

    接口

--- src/com/celink/xieservice/service/user/UService.java
+++ src/com/celink/xieservice/service/user/UService.java
@@ -47,4 +47,110 @@ public interface UService {
 	 * @return
 	 */
 	public String updatePwd(String jsonData);
+	
+	/**
+	 * 获取用户订单接口
+	 * ps：
+	 * 传state=3查找完成的订单，传state=0查找未完成的订单
+	 * @param userId 用户id
+	 * @param state 订单状态
+	 * @return
+	 */
+	public String getUserOrderListByState(int userId, int state);
+	
+	/**
+	 * 获取订单详情接口
+	 * @param orderId 订单id
+	 * @return
+	 */
+	public String getOrderDescByOrderId(int orderId);
+	
+	/**
+	 * 增加订单
+	 * @param jsonData
+	 * {
+	 * userId：int 用户编号
+	 * orderName:String 服务名称(比如，如果是猫的订单，那这个服务名称就是“猫的服务订单”)
+	 * packageServiceId：int 选择的套餐编号
+	 * price：float 小计费用
+	 * serviceAddressId：int 服务地址编号
+	 * serviceTime:String 服务时间
+	 * additionalRequirements:String  附加要求
+	 * isUseVoucher：int 是否使用抵用卷（0、否，1、是）
+	 * sumPrice：float 总费用
+	 * }
+	 * @return
+	 */
+	public String addOrder(String jsonData);
+	
+	/**
+	 * 获取评论列表接口
+	 * @param userId 用户Id
+	 * @param orderId 订单id
+	 * @return
+	 */
+	public String getOrderComments(int userId, int orderId);
+	
+	/**
+	 * 获取用户的服务地址接口
+	 * @param userId 用户Id
+	 * @return
+	 */
+	public String getUserServiceAddress(int userId);
+	
+	/**
+	 * 增加用户服务地址接口
+	 * @param jsonData
+	 * {
+	 *  userId：int 用户Id
+	 *	reservation:String 预约人
+	 *	phone:String 电话
+	 *	districtInformation:String 小区信息
+	 *	addresss:String 详细地址
+	 *	idDefaultServiceAddress:int  是否默认为服务地址（0、否，1、是）
+	 * }
+	 * @return
+	 */
+	public String addUserServiceAddress(String jsonData);
+	
+	/**
+	 * 获取用户的优惠券接口
+	 * @param userId 用户Id
+	 * @param isUsed 是否已使用（0、否，1、是）
+	 * @return
+	 */
+	public String getUserCouponByIsUsed(int userId, int isUsed);
+	
+	/**
+	 * 获取服务套餐列表接口
+	 * ps:
+	 * type=1-->日常保洁
+	 * type=2-->采耳
+	 * type=3-->狗
+	 * type=4-->猫
+	 * @param type 套餐类型
+	 * @return
+	 */
+	public String getServicePackageByType(int type);
+	
+	/**
+	 * 检查APP是否有更新
+	 * @param platform ios/android
+	 * @return
+	 */
+	public String checkVersion(String platform);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/com/celink/xieservice/service/user/impl/UServiceImpl.java
+++ src/com/celink/xieservice/service/user/impl/UServiceImpl.java
@@ -8,7 +8,7 @@ import com.celink.xieservice.app.service.UserService;
 import com.celink.xieservice.service.user.UService;
 @Component
 public class UServiceImpl implements UService {
-private final static Logger logger = Logger.getLogger(UServiceImpl.class);
+	private final static Logger logger = Logger.getLogger(UServiceImpl.class);
 	
 	private UserService userService;
 	
@@ -39,6 +39,60 @@ private final static Logger logger = Logger.getLogger(UServiceImpl.class);
 	@Override
 	public String updatePwd(String jsonData) {
 		return this.userService.updatePwd(jsonData);
+	}
+
+	@Override
+	public String getUserOrderListByState(int userId, int state) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getOrderDescByOrderId(int orderId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String addOrder(String jsonData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getOrderComments(int userId, int orderId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUserServiceAddress(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String addUserServiceAddress(String jsonData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUserCouponByIsUsed(int userId, int isUsed) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getServicePackageByType(int type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String checkVersion(String platform) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	
